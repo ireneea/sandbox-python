44438925c6cdeaf19f0d715ac94d7f0a76c83491
Irenee Ajeneza <irenee.ajeneza@primarybid.com>
2021-09-02 20:29:33 +0100

    refactored and fixed min max loops

--- min-max-loops.py
+++ min-max-loops.py
@@ -1,20 +1,21 @@
 import math
-numbers = [12, -243, 12 , 198, 8, 895, 42, 76, 9]
 
 def get_min(nums):
     min = math.inf
-    for i in numbers:
+    for i in nums:
         if i < min:
             min = i
     return min 
 
 def get_max(nums):
     max = - math.inf
-    for i in numbers:
+    for i in nums:
         if i > max:
             max = i
     return max
 
+
+numbers = [12, -243, 12 , 198, 8, 895, 42, 76, 9]
 print(get_min(numbers))
 print(get_max(numbers))
          
